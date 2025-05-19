abe88eb8a254f2e9f4427bd7c109909c3288257c
ChainsAre2Tight <dima124691@gmail.com>
2025-05-19 17:48:04 +0300

    dried round function

--- pkg/round/round.go
+++ pkg/round/round.go
@@ -11,41 +11,30 @@ func G(h, m, N []uint64) {
 	copy(h_temp, h)
 
 	// pre iterations
-	primitives.X(h, N)
-	primitives.S(h)
-	primitives.P(h)
-	primitives.L(h)
+	xspl(h, N)
 
 	// k1
 	buffer_K := make([]uint64, 8)
 	copy(buffer_K, h)
 
-	primitives.X(h, m)
-	primitives.S(h)
-	primitives.P(h)
-	primitives.L(h)
+	xspl(h, m)
 
 	for round := range 11 {
-		// calc K_i
-		primitives.X(buffer_K, constants.All[round])
-		primitives.S(buffer_K)
-		primitives.P(buffer_K)
-		primitives.L(buffer_K)
-
-		primitives.X(h, buffer_K)
-		primitives.S(h)
-		primitives.P(h)
-		primitives.L(h)
+		xspl(buffer_K, constants.All[round])
+		xspl(h, buffer_K)
 	}
 
 	// k13
-	primitives.X(buffer_K, constants.All[11])
-	primitives.S(buffer_K)
-	primitives.P(buffer_K)
-	primitives.L(buffer_K)
-
+	xspl(buffer_K, constants.All[11])
 	primitives.X(h, buffer_K)
 
 	primitives.X(h, h_temp)
 	primitives.X(h, m)
+}
+
+func xspl(buffer, constant []uint64) {
+	primitives.X(buffer, constant)
+	primitives.S(buffer)
+	primitives.P(buffer)
+	primitives.L(buffer)
 }
