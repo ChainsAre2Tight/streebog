09fa3a9cfef10bd2c2e72cfb9615b8cce640f6ae
ChainsAre2Tight <dima124691@gmail.com>
2025-05-19 12:09:56 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChainsAre2Tight/streebog
+
+go 1.24.3
---
+++ stribog.go
@@ -0,0 +1,9 @@
+package stribog
+
+func Streebog256(message []byte) ([]byte, error) {
+	panic("unimplemented")
+}
+
+func Streebog512(message []byte) ([]byte, error) {
+	panic("unimplemented")
+}
