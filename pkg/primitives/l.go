b21c40f913ae2283f50d85a7b344d6f2dbbaecd0
ChainsAre2Tight <dima124691@gmail.com>
2025-05-20 16:16:50 +0300

    perf: improves L perf by 200%

--- pkg/primitives/l.go
+++ pkg/primitives/l.go
@@ -1,6 +1,10 @@
 package primitives
 
-import "github.com/ChainsAre2Tight/streebog/pkg/tables"
+import (
+	"math/bits"
+
+	"github.com/ChainsAre2Tight/streebog/pkg/tables"
+)
 
 func L(dst []uint64) {
 	for i := range 8 {
@@ -9,14 +13,11 @@ func L(dst []uint64) {
 }
 
 func linear(in uint64) uint64 {
-	var res uint64 = 0
-	index := 63
-	for in > 0 {
-		if in&1 > 0 {
-			res ^= tables.Linear[index]
-		}
-		in >>= 1
-		index--
+	var res uint64
+	for in != 0 {
+		idx := bits.LeadingZeros64(in)
+		res ^= tables.Linear[idx]
+		in &^= 1 << (63 - idx)
 	}
 	return res
 }
